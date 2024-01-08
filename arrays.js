fff5ec618f0e993bf78aaf6865986f49f5a61867
Silvia Avelar <150369573+SilviaAvelar@users.noreply.github.com>
2024-01-08 18:39:11 +0000

    Ex004listadecompras

---
+++ arrays.js
@@ -0,0 +1,18 @@
+// Compras de Supermercado
+
+let supermercado = 'Pingo Doce'
+let temCupom = 'true'
+let cliente = 'Silvia'
+let listaDecompras = ['arroz', 'feijão', 'baycon', 'leite',
+'carne', 'frutas', 'pão', 'cheiro verde']
+
+// Lista de Compras
+
+console.log(listaDecompras)
+
+// Montar frase
+
+console.log(cliente + ' foi ao ' + supermercado +
+' comprar ' + listaDecompras + ' para fazer uma feijoada '
++ cliente + ' usou seu cupom de desconto ' + temCupom +
+' mas não tinha ' + listaDecompras[7])
